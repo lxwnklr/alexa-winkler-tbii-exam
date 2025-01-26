f6a9db73c5d70c2833e183a64aef30d13e91a4db
lxwnklr <winkleralexa@yahoo.com>
2025-01-26 21:26:45 +0100

    Update information.py

--- information.py
+++ information.py
@@ -8,21 +8,26 @@ def information():
     # create the number of columns
     c1, c2 = st.columns(2)
 
-    # first column, display image and use full column width, subheader as hyperlink
+        # first column, display image in container, subheader as hyperlink
     # Hyperlinks: https://discuss.streamlit.io/t/hyperlink-in-streamlit-without-markdown/7046
     with c1:
-        st.image("general.jpg", use_column_width="always")
+        # using containers instead of use_column_width="Always" because this parameter will be removed in a future release
+        general = st.container()
+        general.image("general.jpg")
         st.subheader("[⬆️General info⬆️](https://www.rei.com/learn/expert-advice/cross-country-skiing-for-beginners.html#:~:text=Basic%20Cross-Country%20Ski%20Techniques%201%20The%20Balanced%20Stance,the%20shuffle-and-glide%29%3A%20...%205%20Expand%20Your%20Skills%20)")
 
-        st.image("skating.jpg", use_column_width="always")
+        skating = st.container()
+        skating.image("skating.jpg")
         st.subheader("[⬆️Skating technique⬆️](https://www.rei.com/learn/expert-advice/how-to-skate-ski.html)")
 
-    # second column, display image and use full column width, subheader as hyperlink
+    # second column, display image in container, subheader as hyperlink
     with c2:
-        st.image("waxing.jpg", use_column_width="always")
+        waxing = st.container()
+        waxing.image("waxing.jpg")
         st.subheader("[⬆️Grip-waxing⬆️](https://www.rei.com/learn/expert-advice/grip-waxing-crosscountry-skis.html)")
 
-        st.image("classic.jpg", use_column_width="always")
+        classic = st.container()
+        classic.image("classic.jpg")
         st.subheader("[⬆️Classic technique⬆️](https://www.rei.com/learn/expert-advice/how-to-cross-country-ski.html#cross-country-ski-gear)")
 
     # credit for photos as hyperlink connected to profile of creator
